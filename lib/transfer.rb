bfaf2ba666b667e06bc5dedeed0e16ac80ec7fdc
Michael Kim <m.kim4247@gmail.com>
2020-10-24 21:33:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,11 +9,7 @@ class Transfer
   end
   
   def valid?
-    if @sender.valid? && @receiver.valid?
-      true
-    else
-      false
-    end
+    @sender.valid? && @receiver.valid?
   end
   
   def execute_transaction
