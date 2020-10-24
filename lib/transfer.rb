e7d6ceba416fab5889b26c36aef2a183b86ab503
Michael Kim <m.kim4247@gmail.com>
2020-10-24 21:27:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @receiver.balance += @amount
       @status = 'complete'
     else
-      
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
