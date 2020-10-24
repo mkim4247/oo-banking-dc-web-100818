f64f159263b13a54e01ec9e0ab61e85f6cf8c977
Michael Kim <m.kim4247@gmail.com>
2020-10-24 21:28:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
+    if self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
